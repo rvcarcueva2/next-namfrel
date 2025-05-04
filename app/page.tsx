36fbffc2c58ff238005cc45a489f6f12ea1071f9
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2025-05-04 11:52:45 +0000

    removed h1 tag in pages and added box-shadow in sub-header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 export default function TransmissionPage() {
     return (
       <main className="p-8">
-        <h1 className="text-2xl font-bold">Transmission</h1>
       </main>
     );
   }
-  
--- app/senate/page.tsx
+++ app/senate/page.tsx
@@ -1,7 +1,6 @@
 export default function SenatePage() {
     return (
       <main className="p-8">
-        <h1 className="text-black">Senate</h1>
       </main>
     );
   }
