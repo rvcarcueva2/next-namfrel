ac3b216a342bd8ea9a499d7a1b665497e267998d
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2025-05-08 08:47:12 +0000

    adjusted logo banner on header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,33 +24,38 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
-        <div className="relative z-20 flex -mb-30">
-          <div className="logo-shadow-wrapper">
-            <div className="logo-container">
-              <img
-                src="/images/2025-NAMFREL-logo.png"
-                alt="Eleksyon Logo"
-                className="eleksyon-logo namfrel-logo"
-              />
+          <div className="z-20 flex -mb-30">
+            <div className="logo-shadow-wrapper">
+              <div className="logo-container">
+                <img
+                  src="/images/2025-NAMFREL-logo.png"
+                  alt="Eleksyon Logo"
+                  className="eleksyon-logo namfrel-logo"
+                />
 
-              <img
-                src="/images/APC-Gold_Site-Seal.png"
-                alt="APC Logo"
-                className="eleksyon-logo apc-logo"
-              />
+                <img
+                  src="/images/APC-Gold_Site-Seal.png"
+                  alt="APC Logo"
+                  className="eleksyon-logo apc-logo"
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="header-container">
-          <Header />
-          <SubHeader />
-        </div>
-
+          <div className="flex-1">
+            <div className="logo-spacer hidden md:block" />
+              <div className="header-container relative z-10"> 
+                <Header />
+                <SubHeader />
+              </div>
+          </div>
+          
+        {/* Main content */}
         {children}
 
-        <Footer />
-      </body>
-    </html>
+      <Footer />
+        
+    </body>
+  </html>
   );
 }
