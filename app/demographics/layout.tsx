5a3696544a59289624de34e9e4882ec0e2005b22
kypantino <kylaamorpantino@gmail.com>
2025-05-09 15:00:21 +0000

    Updates from feature/18 are added and updated

--- app/demographics/layout.tsx
+++ app/demographics/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Crowdsourcing | NAMFREL",
+  title: "Demographics | NAMFREL",
   description: "NAMFREL Website",
 };
 
--- app/election-statistics/layout.tsx
+++ app/election-statistics/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Election Statistics| NAMFREL",
+  title: "Election Statistics | NAMFREL",
   description: "NAMFREL Website",
 };
 
